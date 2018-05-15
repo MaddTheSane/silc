404d8a49f82d68e36756c6ae8294dea0ccdd1857
C.W. Betts <computers57@hotmail.com>
2018-05-14 21:56:54 -0600

    Add Xcode frameworks project.

---
+++ xcode/libsilc/libsilc.h
@@ -0,0 +1,19 @@
+//
+//  libsilc.h
+//  libsilc
+//
+//  Created by C.W. Betts on 5/14/18.
+//  Copyright © 2018 C.W. Betts. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+//! Project version number for libsilc.
+CF_EXPORT double libsilcVersionNumber;
+
+//! Project version string for libsilc.
+CF_EXPORT const unsigned char libsilcVersionString[];
+
+#include <libsilc/silc.h>
+
+
---
+++ xcode/libsilcclient/libsilcclient.h
@@ -0,0 +1,18 @@
+//
+//  libsilcclient.h
+//  libsilcclient
+//
+//  Created by C.W. Betts on 5/14/18.
+//  Copyright © 2018 C.W. Betts. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+//! Project version number for libsilcclient.
+CF_EXPORT double libsilcclientVersionNumber;
+
+//! Project version string for libsilcclient.
+CF_EXPORT const unsigned char libsilcclientVersionString[];
+
+#include <libsilc/libsilc.h>
+#include <libsilcclient/silcclient.h>
